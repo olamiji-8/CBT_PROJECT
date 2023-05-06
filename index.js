63c31b0ca02429b4106d03121ea1ce05f687f8b1
olamiji-8 <olamijisotomi@gmail.com>
2023-05-06 02:53:57 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const router = require("./router/route");
 
 const port = process.env.PORT 
 const cors = require("cors");
